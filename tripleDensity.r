c6d1e5c45ccd50b0fa9f470447c23f2d6d7f6f73
bassopenguin <bonescontra06@gmail.com>
2017-05-17 23:00:13 -0400

    Pass lintr by shortening lines

--- tripleDensity.r
+++ tripleDensity.r
@@ -131,7 +131,12 @@ print(theplot)
 # if desired, print High/Low medians for short and long periods
 statslogging <- config$statslogging
 if (statslogging == TRUE) {
-  print(paste("Short:", format(short, digits = 0, nsmall = 0), data$Date[short], sep = " "))
+  print(paste("Short:",
+    format(short,
+           digits = 0,
+           nsmall = 0),
+    data$Date[short],
+    sep = " "))
   print(paste("  High",
     format(
       median(
@@ -145,7 +150,12 @@ if (statslogging == TRUE) {
         head(data$Low, short)),
         digits = 2, nsmall = 2),
         sep = "  $"))
-  print(paste("Long:", format(long, digits = 0, nsmall = 0), data$Date[long], sep = " "))
+  print(paste("Long:",
+    format(long,
+           digits = 0,
+           nsmall = 0),
+    data$Date[long],
+    sep = " "))
   print(paste("  High",
     format(
       median(
